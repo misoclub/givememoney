a88713b4c93d1aadc4f4b9992b00d84706defa0b
Takafumi Naito <takafumi.naito@dena.com>
2022-09-12 15:53:54 +0900

    更新

--- js/main.js
+++ js/main.js
@@ -1,22 +1,12 @@
-
 isStarted = false;
-
 startdate = new Date();
 
 // 時給。
 jikyuu = 5000;
 second = jikyuu / 60 / 60;
 
-function getParam(name, url)
-{
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-    results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
+timerId = 0;
+money = 0;
 
 // Webからのコピペ。日付フォーマット。
 function dateToStr24HPad0DayOfWeek(date, format) {
@@ -36,33 +26,26 @@ function dateToStr24HPad0DayOfWeek(date, format) {
 
 function load() {
     var saveData = store.get('user_data');
-
     // データが存在するならせっせとフォームにセットしにいく。
     if (!saveData) {
         return;
     }
-
-
-    if(saveData["jikyu"] && saveData["jikyu"] != "")
-    {
+    if (saveData["jikyu"] && saveData["jikyu"] != "") {
         $('#jikyu').val(saveData["jikyu"]);
     }
-
     // すでに開始している。
-    if(saveData["enable"] && saveData["enable"] != "")
-    {
-        if(saveData["enable"])
-        {
-startWork(0);
+    if (saveData["enable"] && saveData["enable"] != "") {
+        if (saveData["enable"]) {
+            startWork(saveData["time"]);
+            isStarted = true;
+            $('#start').hide();
+            $('#stop').show();
         }
     }
-
-
 }
 
 function save(_time, _enable, _jikyuu) {
     var saveData = {};
-    // saveData["yourname"] = yourname;
     saveData["time"] = _time.getTime();
     saveData["enable"] = _enable;
     saveData["jikyu"] = _jikyuu;
@@ -70,78 +53,67 @@ function save(_time, _enable, _jikyuu) {
 }
 
 function initialize() {
-    // // 今日のいい感じの日時を入れておく。
-    // var nowdate = new Date();
-    // // デフォルト今日の日付。
-    // var day = dateToStr24HPad0DayOfWeek(nowdate, 'YYYY年MM月DD日(WW)');
-    // $('#targetDate').val(day);
-
     // 前回のデータ読み込み。
     load();
 }
 
-function startWork(startTime)
-{
-        // 開始してたらスルー。
-        if(isStarted)
-        {
-            return;
-        }
+function startWork(startTime) {
+    const countUp = () => {
+        // 経過時間。
+        $('#timeText').text(passTime(startdate));
+        // 稼いだお金。
+        var diff_time = Date.now() - startdate.getTime();
+        var seconds = Math.floor(diff_time / 1000);
+        money = Math.floor(second * seconds * 100) / 100;
+        $('#moneyText').text(money + "円");
+    }
 
-        let count = 0;
-        const countUp = () => {
-            count++;
+    // フォームに入力された値を取得。
+    jikyuu = $('#jikyu').val();
 
-            // 経過時間。
-            $('#timeText').text(passTime(startdate));
+    if (isNaN(jikyuu)) {
+        alert("入力された時給が数値ではありません");
+        return;
+    } else if (jikyuu == "") {
+        alert("時給を入力してください");
+        return;
+    }
 
-            // 稼いだお金。
-            var money = Math.floor(second * count * 100) / 100;
-            $('#moneyText').text(money+"円");
-        }
+    // 秒給を計算。
+    second = jikyuu / 60 / 60;
 
-        jikyuu = $('#jikyu').val();
-
-        if(isNaN(jikyuu))
-        {
-            alert("入力された時給が数値ではありません");
-            return;
-        }
-        else if(jikyuu == "")
-        {
-            alert("時給を入力してください");
-            return;
-        }
-
-        second = jikyuu / 60 / 60;
-
-        if(startTime == 0)
-        {
+    if (startTime == 0) {
         startdate = new Date();
-        }
-        else
-        {
+    } else {
+        startdate = new Date(startTime);
+    }
 
-        }
-        var day = dateToStr24HPad0DayOfWeek(startdate, 'YYYY年MM月DD日(WW) hh:mm');
-        $('#startTimeText').text(day);
+    // もろもろ値初期化。
+    var diff_time = Date.now() - startdate.getTime();
+    var seconds = Math.floor(diff_time / 1000);
+    money = Math.floor(second * seconds * 100) / 100;
+    $('#moneyText').text(money + "円");
 
-        setInterval(countUp, 1000);
-        isStarted = true;
+    $('#timeText').text(passTime(startdate));
+    
+    // 開始日時。
+    var day = dateToStr24HPad0DayOfWeek(startdate, 'YYYY年MM月DD日(WW) hh:mm');
+    $('#startTimeText').text(day);
+
+    timerId = setInterval(countUp, 1000);
+    isStarted = true;
 }
 
-function stopWork()
-{
-
+function stopWork() {
+    clearInterval(timerId);
+    isStarted = false;
+    alert("稼いだお金は" + money + "円でした。\nおつかれさまでした！");
 }
 
 // 経過時間のテキストを返す。
-function passTime(startTime)
-{
+function passTime(startTime) {
     var diff_time = Date.now() - startTime.getTime();
-
     var seconds = Math.floor(diff_time / 1000);
-
     var pass_seconds = seconds % 60;
     var pass_minutes = Math.floor(seconds / 60) % 60;
     var pass_hours = Math.floor(seconds / (60 * 60));
@@ -149,33 +121,30 @@ function passTime(startTime)
     return ('0' + pass_hours).slice(-2) + ":" + ('0' + pass_minutes).slice(-2) + ":" + ('0' + pass_seconds).slice(-2);
 }
 
-$(function() {
-
+$(function () {
     // 開始ボタン。
-    $('#start').click(function() {
-
-        startWork(0);
-
-        save(startdate, true, jikyuu);
-    });
-
-    $('#stop').click(function() {
-        // 開始してなかったらスルー。
-        if(!isStarted)
-        {
+    $('#start').click(function () {
+        // 開始してたらスルー。
+        if (isStarted) {
             return;
         }
-
-        stopWork(0);
-
-        save(startdate, false, jikyuu);
+        startWork(0);
+        save(startdate, true, jikyuu);
+        $('#start').hide();
+        $('#stop').show();
     });
-
-
-
-
+    // 終了ボタン。
+    $('#stop').click(function () {
+        // 開始してなかったらスルー。
+        if (!isStarted) {
+            return;
+        }
+        stopWork();
+        save(startdate, false, jikyuu);
+        $('#start').show();
+        $('#stop').hide();
+    });
 });
-
 
 
 
